8804731fcf83f71604689660cc735972c9c7e5d5
Hasannm17 <118686880+Hasannm17@users.noreply.github.com>
2023-09-15 11:27:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const {User}=require('./database/Users');
 const auth = require("./auth");
 const bycrypt=require('bcrypt');
 const jwt =require('jsonwebtoken');
+const fs  = require('fs');
+const qr =require('qr-image')
+const mongoose = require('mongoose');
+const { log } = require('console');
 const PORT = process.env.PORT | 3000 ;
 const app = express();
 require('dotenv').config();
 app.use(express.json());
 app.use(BodyParser.urlencoded({extended:true}));
 app.use(cors());
-app.listen(PORT , ()=>{console.log(`Running on port ${PORT}`)});
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,9 +28,40 @@ app.use((req, res, next) => {
     );
     next();
   });
+app.listen(PORT , ()=>{console.log(`Running on port ${PORT}`)});
+const qrCodeSchema = new mongoose.Schema({
+  imageData: Buffer, // Store image data as a buffer
+  createdAt: { type: Date, default: Date.now },
+});
+
+const QRCodeModel = mongoose.model('QRCode', qrCodeSchema);
+
+
   
   app.route('/').get((req ,res)=>{
     res.send('Welcome to our server')
+  }).post((req,res )=>{
+    const { url } = req.body;
+
+    // Generate the QR code image
+    const qrImage = qr.image(url, { type: 'png' });
+  
+    // Read the QR code image into a Buffer
+    const qrImageData = [];
+    qrImage.on('data', (data) => {
+      qrImageData.push(data);
+    });
+  
+    qrImage.on('end', () => {
+      // Concatenate the data and send it as a response
+      const qrImageBuffer = Buffer.concat(qrImageData);
+  
+      // Set the appropriate content type
+      res.setHeader('Content-Type', 'image/png');
+  
+      // Send the QR code image as a response
+      res.send(qrImageBuffer);
+    });
   })
 
 app.route('/signup').get((req, res )=>{
